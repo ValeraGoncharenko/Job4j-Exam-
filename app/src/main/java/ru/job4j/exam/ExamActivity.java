947dbd0cdb121ff87c0e30d8b58a9567ee30cad5
Goncharenko Valery <goncharikvv@gmail.com>
2021-04-13 23:48:18 +0300

    1. Activity. Жизненный цикл.[61528#398138]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -1,15 +1,67 @@
 package ru.job4j.exam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ExamActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyLogs";
+
+    static final String ROTATION = "Rotation";
+    private static int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
-       // setContentView(R.layout.custom_layout);
+        // setContentView(R.layout.custom_layout);
+        if (savedInstanceState != null) {
+          savedInstanceState.getInt(ROTATION);
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "OnPause");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(ROTATION, count++);
+        Log.i(TAG, "count: " + count);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "OnDestroy");
+    }
+
+
+
+
 }
