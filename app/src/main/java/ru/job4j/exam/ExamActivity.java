2c42cd4367f4fdfa51ca43a85342d5d2111f58d9
Goncharenko Valery <goncharikvv@gmail.com>
2021-05-16 23:12:00 +0300

    2.2. Зависимости компонентов. [#398145]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -35,29 +35,7 @@ public class ExamActivity extends AppCompatActivity {
     private List<Integer> answer = new ArrayList<>();
 
     /**Поле содержащее список вопросов*/
-    private final List<Question> questions = Arrays.asList(
-            new Question(
-                    1, "How many primitive variables does Java have?",
-                    Arrays.asList(
-                            new Option(1, "1.1"), new Option(2, "1.2"),
-                            new Option(3, "1.3"), new Option(4, "1.4")
-                    ), 4
-            ),
-            new Question(
-                    2, "What is Java Virtual Machine?",
-                    Arrays.asList(
-                            new Option(1, "2.1"), new Option(2, "2.2"),
-                            new Option(3, "2.3"), new Option(4, "2.4")
-                    ), 4
-            ),
-            new Question(
-                    3, "What is happen if we try unboxing null?",
-                    Arrays.asList(
-                            new Option(1, "3.1"), new Option(2, "3.2"),
-                            new Option(3, "3.3"), new Option(4, "3.4")
-                    ), 4
-            )
-    );
+    private final QuestionStore store = QuestionStore.getInstance();
 
     /**Поле указывающее на текущий вопрос*/
     private int position = 0;
@@ -67,9 +45,9 @@ public class ExamActivity extends AppCompatActivity {
      */
     private void fillForm() {
         findViewById(R.id.previous).setEnabled(position != 0);
-        findViewById(R.id.next).setEnabled(position != questions.size() - 1);
+        findViewById(R.id.next).setEnabled(position != store.size() - 1);
         final TextView text = findViewById(R.id.question);
-        Question question = this.questions.get(this.position);
+        Question question = this.store.get(this.position);
         text.setText(question.getText());
         variants = findViewById(R.id.variants);
         for (int index = 0; index != variants.getChildCount(); index++) {
@@ -86,7 +64,7 @@ public class ExamActivity extends AppCompatActivity {
     private void showAnswer() {
         variants = findViewById(R.id.variants);
         int id = variants.getCheckedRadioButtonId();
-        Question question = this.questions.get(this.position);
+        Question question = this.store.get(this.position);
         Toast.makeText(
                 this, "Your answer is " + id + ", correct is " + question.getAnswer(),
                 Toast.LENGTH_SHORT
